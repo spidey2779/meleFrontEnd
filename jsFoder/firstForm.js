26f54cb364b1449b55d0a09f68680af0b3724257
spidey2779 <gteja2779@gmail.com>
2023-11-25 12:52:19 +0530

    changes added

---
+++ jsFoder/firstForm.js
@@ -0,0 +1,21 @@
+ //loader
+  // Function to start the loader animation
+  function startLoader() {
+    const loader = document.querySelector('.circle-container');
+    loader.style.display = 'flex';
+    const backdrop = document.querySelector('.backdrop');
+    backdrop.style.display = 'block'; // Show backdrop
+    // Disable scrolling
+    document.body.style.overflow = 'hidden';
+  }
+
+  // Function to stop the loader animation
+  function stopLoader() {
+    const loader = document.querySelector('.circle-container');
+    loader.style.display = 'none';
+    const backdrop = document.querySelector('.backdrop');
+    backdrop.style.display = 'none'; // Hide backdrop
+
+    // Enable scrolling
+    document.body.style.overflow = 'auto';
+  }
---
+++ loginfolder/app.js
@@ -0,0 +1,23 @@
+const openeye= document.getElementById('openeye')
+const closedeye=document.getElementById('closedeye')
+pinput=document.getElementById('passwordinput');
+openeye.addEventListener('click', function(){
+     closedeye.classList.toggle('inactive')
+     openeye.classList.toggle('inactive')
+     if(pinput.type == 'password'){
+         pinput.type='text';
+     }
+     else{
+         pinput.type='password'
+     }
+ })
+closedeye.addEventListener('click', function(){
+     openeye.classList.toggle('inactive')
+     closedeye.classList.toggle('inactive')
+     if(pinput.type == 'password'){
+         pinput.type='text';
+     }
+     else{
+         pinput.type='password'
+     }
+ })

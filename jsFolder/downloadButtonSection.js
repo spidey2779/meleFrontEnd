9edda1db5ec8c44c8b286f4e9f67a391075fdea2
spidey2779 <gteja2779@gmail.com>
2023-11-29 23:46:19 +0530

    pdf successfull

--- jsFolder/downloadButtonSection.js
+++ jsFolder/downloadButtonSection.js
@@ -124,3 +124,127 @@ document.getElementById('AdminSheetBtn').addEventListener('click', function() {
     generateCSV(mydata);
   });
   
+
+
+
+  // generating pdf for invizilators
+
+  // Function to generate PDF
+function generatePDF() {
+  // Function to generate details text
+  function generateDetailsText(detailsArray) {
+    return detailsArray
+      .map(
+        detail =>
+          `${detail.roll}\n${detail.department}\n${detail.subCode}\n`
+      )
+      .join('\n\n');
+  }
+
+  // Initialize a new jsPDF instance
+  const pdf = new jsPDF();
+
+  // Find all unique room numbers in the data
+  const uniqueRoomNumbers = [...new Set(mydata.flatMap(roomData => Object.keys(roomData)))];
+
+  // Iterate through each unique room number
+  uniqueRoomNumbers.forEach(roomNumber => {
+    const detailsSet = mydata.reduce((acc, roomData) => {
+      // Check if the room number is present in the current object
+      if (roomNumber in roomData) {
+        // Concatenate the details for the current room number
+        return acc.concat(roomData[roomNumber]);
+      }
+      return acc;
+    }, []);
+
+    // Set up the dimensions and spacing for the page
+    const pageWidth = pdf.internal.pageSize.width;
+    const pageHeight = pdf.internal.pageSize.height;
+    const margin = 12;
+    const boxWidth = (pageWidth - 2 * margin) / 4;
+    const boxHeight = (pageHeight - 2 * margin) / 6;
+
+    // Draw the big box for the room
+    pdf.rect(margin, margin, pageWidth - 2 * margin, pageHeight - 2 * margin);
+
+    // Set font size
+    const fontSize = 18;
+    pdf.setFontSize(fontSize);
+
+    // Draw room number as heading
+    pdf.text(pageWidth / 2, 5, roomNumber, {
+      align: 'center',
+      baseline: 'top',
+    });
+    pdf.setFontSize(fontSize - 8)
+
+    // Draw small boxes and insert details
+    for (let row = 0; row < 6; row++) {
+      for (let col = 0; col < 4; col++) {
+        const x = margin + col * boxWidth;
+        const y = margin + row * boxHeight;
+
+        // Calculate the seat number based on column parity
+        let seatNumber;
+        if (col % 2 === 0) {
+          // For odd columns, start from (col * 6) + row + 1 and increment
+          seatNumber = col * 6 + row + 1;
+        } else {
+          // For even columns, start from (col + 1) * 6 - row and decrement
+          seatNumber = (col + 1) * 6 - row;
+        }
+
+        // Reverse the order of seat numbers in the second and fourth columns
+        if (col === 1 || col === 3) {
+          seatNumber = (col + 1) * 6 - row;
+        }
+
+        // Draw small box
+        pdf.rect(x, y, boxWidth, boxHeight);
+        pdf.setFontSize(fontSize - 3)
+
+        // Display seat number at the top
+        pdf.text(x + boxWidth / 1.3, y + boxHeight / 4, seatNumber.toString(), {
+          align: 'center',
+          baseline: 'middle',
+          angle: 0,
+        });
+        pdf.setFontSize(fontSize - 8)
+
+        // Check if seat number matches and insert details at the bottom
+        const matchingDetails = detailsSet.filter(detail => detail.seatNo === seatNumber);
+
+        // Generate details text for matching details
+        const detailsText = generateDetailsText(matchingDetails);
+
+        // Adjust margin and padding to fit within the box
+        const textMargin = 2;
+        // const textPadding = 2;
+        const maxWidth = boxWidth - 2 * textMargin;
+
+        // Draw details text horizontally within the same box
+        pdf.text(
+          x + textMargin,
+          y + boxHeight / 4,
+          pdf.splitTextToSize(detailsText, maxWidth),
+          {
+            align: 'left',
+            baseline: 'top',
+            angle: 0,
+          }
+        );
+      }
+    }
+
+    // Add a new page for the next room
+    pdf.addPage();
+  });
+
+  // Save or display the PDF
+  pdf.save('output.pdf');
+}
+
+document.getElementById('InvizilatorSheetsBtn').addEventListener('click', function (e) {
+  generatePDF();
+});
